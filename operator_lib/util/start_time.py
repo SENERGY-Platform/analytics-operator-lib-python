dca908c8d07facb9e1fc8c8c2726f86e5e5c7c16
Hannes Hansen <mail@hannesh.de>
2024-04-23 12:02:31 +0200

    fix ts print

--- operator_lib/util/start_time.py
+++ operator_lib/util/start_time.py
@@ -4,6 +4,7 @@ __all__ = ("setup_operator_starttime", )
 
 from operator_lib.util.persistence import load, save
 from operator_lib.util.logger import logger
+from operator_lib.util.timestamps import timestamp_to_str
 
 FILE_NAME_OPERATOR_START_TIME = "operator_start_time.pickle"
 
@@ -23,6 +24,6 @@ def setup_operator_starttime(data_path):
         operator_start_time = datetime.datetime.now(datetime.timezone.utc)
         logger.info(f"Store operator start time not found -> create and save")
         save_operator_start_time(data_path, operator_start_time)
-    logger.info(f"Operator start time: {operator_start_time}")
+    logger.info(f"Operator start time: {timestamp_to_str(operator_start_time)}")
     return operator_start_time
 
