9a6f6939fb74fb074b9d0b6da7949831de696634
Hannes Hansen <mail@hannesh.de>
2024-05-06 12:45:17 +0200

    use first data time instead of operator start time

--- operator_lib/util/init_phase.py
+++ operator_lib/util/init_phase.py
@@ -1,6 +1,5 @@
 from operator_lib.util.persistence import load, save
 from operator_lib.util.logger import logger
-from operator_lib.util.start_time import load_operator_start_time
 
 __all__ = ("InitPhase", )
 
@@ -16,12 +15,12 @@ class InitPhase():
         self.data_path = data_path
         self.init_phase_duration = init_phase_duration
         self.__load_state()
-        self.operator_start_time = load_operator_start_time(data_path)
-        if not self.operator_start_time:
-            raise Exception("Operator Start Time File is missing!")
+        self.first_data_time = load(data_path, "first_data_time.pickle")
+        if not self.first_data_time:
+            raise Exception("First Data Time File is missing!")
 
     def generate_init_msg(self, timestamp, value_dict):
-        td_until_start = self.init_phase_duration - (timestamp - self.operator_start_time)
+        td_until_start = self.init_phase_duration - (timestamp - self.first_data_time)
         minutes_until_start = int(td_until_start.total_seconds()/60)
         return self.__create_message(value_dict, minutes_until_start)
     
@@ -47,9 +46,9 @@ class InitPhase():
 
     def operator_is_in_init_phase(self, timestamp):
         # sometimes timestamp might be in wrong order, which could trigger an acitve init phase
-        init_active = timestamp-self.operator_start_time < self.init_phase_duration and not self.init_phase_resetted
+        init_active = timestamp-self.first_data_time < self.init_phase_duration and not self.init_phase_resetted
         if init_active:
-            logger.debug(f"Still in initialisation phase! {timestamp} - {self.operator_start_time} < {self.init_phase_duration}")
+            logger.debug(f"Still in initialisation phase! {timestamp} - {self.first_data_time} < {self.init_phase_duration}")
         return init_active
 
     def init_phase_needs_to_be_reset(self):
