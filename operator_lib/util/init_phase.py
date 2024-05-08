b6a944a9da47b67618f5fd9ad5fb3ecbfd94d849
Hannes Hansen <mail@hannesh.de>
2024-05-08 13:31:51 +0200

    added produce function

--- operator_lib/util/init_phase.py
+++ operator_lib/util/init_phase.py
@@ -11,23 +11,24 @@ class InitPhase():
         self, 
         data_path,
         init_phase_duration,
-        first_data_time
+        first_data_time,
+        produce
     ):
         self.data_path = data_path
         self.init_phase_duration = init_phase_duration
         self.__load_state()
         self.first_data_time = first_data_time
+        self.produce = produce
 
     def generate_init_msg(self, timestamp, value_dict):
         td_until_start = self.init_phase_duration - (timestamp - self.first_data_time)
         minutes_until_start = int(td_until_start.total_seconds()/60)
         return self.__create_message(value_dict, minutes_until_start)
     
-    def first_init_msg_needs_to_send(self):
-        return not self.init_phase_resetted and not self.init_phase_was_sent
-
-    def generate_first_init_msg(self, value_dict):
-        return self.__create_message(value_dict)
+    def send_first_init_msg(self, value_dict):
+        if not self.init_phase_resetted and not self.init_phase_was_sent:
+            init_msg = self.__create_message(value_dict)
+            self.produce(init_msg)
 
     def __create_message(self, value_dict, minutes_until_start=None):
         if not minutes_until_start:
@@ -38,10 +39,7 @@ class InitPhase():
 
     def __load_state(self):
         self.init_phase_resetted = load(self.data_path, FILE_NAME_INIT_PHASE_RESET)
-        self.init_phase_was_sent = load(self.data_path, FILE_NAME_INIT_PHASE_SENT)
-
-    def __save_init_phase_was_resetted(self):
-        save(self.data_path, FILE_NAME_INIT_PHASE_RESET, True)
+        self.init_phase_was_sent = load(self.data_path, FILE_NAME_INIT_PHASE_SENT)        
 
     def operator_is_in_init_phase(self, timestamp):
         # sometimes timestamp might be in wrong order, which could trigger an acitve init phase
@@ -56,6 +54,6 @@ class InitPhase():
     def reset_init_phase(self, value_dict):
         logger.debug("Reset init phase message")
         self.init_phase_resetted = True
-        self.__save_init_phase_was_resetted()
+        save(self.data_path, FILE_NAME_INIT_PHASE_RESET, True)
         value_dict["initial_phase"] = ""
         return value_dict
