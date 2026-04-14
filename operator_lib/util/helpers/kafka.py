bb3c7e086e28adcdc3494a7d783fe7df514ccacb
franzmueller <franzmueller@users.noreply.github.com.>
2026-04-14 07:34:37 +0200

    fix kafka dataset timestamp evaluation

--- operator_lib/util/helpers/kafka.py
+++ operator_lib/util/helpers/kafka.py
@@ -19,7 +19,8 @@ def get_kafka_dataset(bootstrap: str, input_topic: InputTopic, pipeline_id: str,
                                        f_value=input_topic.filterValue, pipeline_id=pipeline_id)
 
     def __filter_kafka_msg(msg: dict) -> bool:
-        if datetime.datetime.fromtimestamp(msg["timestamp"]) < cutoff:
+        msg_timestamp = datetime.datetime.fromtimestamp(msg["timestamp"] / 1000.0)
+        if msg_timestamp < cutoff:
             return False        
         payload = json.loads(msg["value"])
         for f in filter:
@@ -30,7 +31,8 @@ def get_kafka_dataset(bootstrap: str, input_topic: InputTopic, pipeline_id: str,
     ds = ray.data.read_kafka(bootstrap_servers=bootstrap, topics=input_topic.name).filter(__filter_kafka_msg)
     if require_full_duration:
         msg = ds.take(1)
-        time.sleep(max(0, (cutoff - datetime.datetime.fromtimestamp(msg["timestamp"])).total_seconds()))
+        msg_timestamp = datetime.datetime.fromtimestamp(msg["timestamp"] / 1000.0)
+        time.sleep(max(0, (cutoff - msg_timestamp).total_seconds()))
     return ds
 
 
