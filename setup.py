2935f3d6f0c9d5a4b576d68e0fd2edbb8a68fe4a
franzmueller <franzmueller@users.noreply.github.com.>
2026-04-15 07:55:29 +0200

    pin structlog version

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
         'ray[data]==2.54.1',
         'ray[client]==2.54.1',
         'psycopg2-binary==2.9.11',
-        'structlog @ git+https://github.com/SENERGY-Platform/python-structlog.git'
+        'structlog @ git+https://github.com/SENERGY-Platform/python-structlog.git@3e2741a164cc41242daac9e5f887a29925fea06f'
     ],
     packages=setuptools.find_packages(),
     python_requires='>=3.5.3',
