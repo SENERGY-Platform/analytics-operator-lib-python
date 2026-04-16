41bae0209f64e18ea123aea9fe9b41266cbaa0dd
franzmueller <franzmueller@users.noreply.github.com.>
2026-04-16 10:28:56 +0200

    fix outdated log message

--- operator_lib/util/op_ml.py
+++ operator_lib/util/op_ml.py
@@ -124,7 +124,7 @@ class MLOperator(OperatorBase):
 
         with self.__mlflow_logger.trace("ray init"):
             logger.debug(
-                f"Initializing Ray with config: {self.config.ray_runtime_env}. This might take a while, even if you see log messages below...")
+                f"Initializing Ray. This might take a while, even if you see log messages below...")
             ray.init(address=self.config.ray_url)
         with self.__mlflow_logger.trace("train"):
             model = self.train(self.model, self.__mlflow_logger)
